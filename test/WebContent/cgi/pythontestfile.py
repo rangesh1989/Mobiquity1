79779462ac81a1d866e1c99696081887d1917403
rangesh1989 <rsundararajan@visustech.com>
2014-12-16 21:58:56 -0800

    Revert "Signed-off-by: rangesh1989 <rsundararajan@visustech.com>"
    This reverts commit 8b17b7ea00581822beca8a83964143ab3a2fdfad.

---
+++ test/WebContent/cgi/pythontestfile.py
@@ -0,0 +1,7 @@
+#!/Python34/ Python
+import os
+import cgitb
+
+print ("<Content-type: text/html>\n\n")
+print ("Hello, world!\n")
+print
---
+++ testpython1/test/pythontestfile.py
@@ -0,0 +1,6 @@
+from os import * 
+from cgi import *
+
+
+print ("Content-type: text/html\n\n");
+print ("Hello, world!\n")
